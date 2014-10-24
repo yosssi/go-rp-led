95bcebfc941acee0b3989574e1f1dab7e3f737f2
yosssi <yoshida.keiji.84@gmail.com>
2014-10-24 14:34:49 +0000

    Add main.go

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		os.Stdout.WriteString("GPIO No should be specified.\n")
+		os.Exit(1)
+	}
+
+	gpio := os.Args[1]
+
+	f, err := os.Create("/sys/class/gpio/export")
+
+	if err != nil {
+		panic(err)
+	}
+
+	f.WriteString(gpio)
+
+	f, err = os.Create(fmt.Sprintf("/sys/class/gpio/gpio%s/direction", gpio))
+
+	if err != nil {
+		panic(err)
+	}
+
+	f.WriteString("out")
+
+	f, err = os.Create(fmt.Sprintf("/sys/class/gpio/gpio%s/value", gpio))
+
+	if err != nil {
+		panic(err)
+	}
+
+	f.WriteString("1")
+
+	time.Sleep(1 * time.Second)
+
+	f.WriteString("0")
+}
